a671591058c897172c4054737b323322e0570aa4
Fionalan727 <lanyufei727@gmail.com>
2019-08-27 13:24:35 -0400

    add routing path to landing page

--- fiona-portfolio/src/App.js
+++ fiona-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
-
+import Main from './component/main'
 class App extends Component {
   render() {
     return (
@@ -25,6 +25,7 @@ class App extends Component {
             </Drawer>
             <Content>
                 <div className="page-content" />
+                <Main/>
             </Content>
         </Layout>
     </div>
--- fiona-portfolio/src/component/main.js
+++ fiona-portfolio/src/component/main.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Landing from './landingPage';
 
+const Main = () => (
+ <Switch>
+     <Route exact path="/" component={Landing}/>
+ </Switch>
+)
+
+export default Main;
