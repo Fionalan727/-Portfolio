b0366d9d7bef3c7ffb8f17bc7a46900a0a0b67c6
Fionalan727 <lanyufei727@gmail.com>
2019-08-27 13:12:54 -0400

    npm installed react-router-dom

--- fiona-portfolio/src/index.js
+++ fiona-portfolio/src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 import './index.css';
 import 'react-mdl/extra/material.css';
 import'react-mdl/extra/material.js';
+import {BrowserRouter} from 'react-router-dom';
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  ,
+  document.getElementById('root'));
