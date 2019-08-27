44d14b8713333565d89f409abd3c1e9b8bb307b4
Fionalan727 <lanyufei727@gmail.com>
2019-08-27 13:47:07 -0400

    add hyperlinks to pages

--- fiona-portfolio/src/App.js
+++ fiona-portfolio/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import './App.css';
-import Main from './component/main'
+import Main from './component/main';
+import{ Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
@@ -9,10 +11,10 @@ class App extends Component {
         <Layout>
             <Header title="Title" scroll>
                 <Navigation>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
-                    <a href="#">Link</a>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/about">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
                 </Navigation>
             </Header>
             <Drawer title="Title">
