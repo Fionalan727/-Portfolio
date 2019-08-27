e30dbecfcd0dbf0905c0b11a6b96dfcc7fc2b7f3
Fionalan727 <lanyufei727@gmail.com>
2019-08-27 13:39:16 -0400

    add all the routes

--- fiona-portfolio/src/component/main.js
+++ fiona-portfolio/src/component/main.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './landingPage';
+import About from './aboutMe';
+import Projects from './project';
+import Contact from './contact';
+import Resume from './resume';
 
 const Main = () => (
  <Switch>
      <Route exact path="/" component={Landing}/>
+     <Route path="/projects" component={Projects}/>
+     <Route path="/about" component={About}/>
+     <Route path="/contact" component={Contact}/>
+     <Route path="/resume" component={Resume}/>
+
  </Switch>
-)
+)   
 
 export default Main;
