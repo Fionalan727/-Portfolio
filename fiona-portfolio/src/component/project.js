d1dcf1b74b33e65d49c555ed83b3b47a24e2a178
Fionalan727 <lanyufei727@gmail.com>
2019-08-28 14:27:55 -0400

    add project categories

--- fiona-portfolio/src/component/project.js
+++ fiona-portfolio/src/component/project.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: 0
+        };
+    }
     render(){
         return(
-            <div>
-                <h1> Project Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab: tabId})} ripple>
+                    <Tab>Express</Tab>
+                    <Tab>React</Tab>
+                    <Tab>Bootstrap/API</Tab>
+                    <Tab>Ruby on Rails</Tab>
+                </Tabs>
             </div>
         )
     }
