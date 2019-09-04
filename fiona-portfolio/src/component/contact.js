21f89d97e7709b9680f8e52fdfbe6f42feae9224
Fionalan727 <lanyufei727@gmail.com>
2019-09-03 23:58:04 -0400

    add contact title

--- fiona-portfolio/src/component/contact.js
+++ fiona-portfolio/src/component/contact.js
@@ -10,11 +10,14 @@ class Contact extends Component {
                         <h2>Yufei Lan</h2>
                         <img src="./images/profile.jpg"
                             alt="avatar"
-                            style={{height:'307px',borderRadius:'8%'}}
+                            style={{height:'307px',borderRadius:'8%', marginTop:'13px'}}
                         />
                         <p style={{width:'75%',margin:'auto',paddingTop:'1em'}}>Full stack developer who is passionate about problem solving and logical puzzles . Enjoys building awesome websites that combine functionality with efficiency to create an intuitive and beautiful user experience. Strong team player driven by results with efficiency. </p>
                     </Cell>
-                    <Cell col={6}>Half Page</Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+                    </Cell>
 
                </Grid>
             </div>
