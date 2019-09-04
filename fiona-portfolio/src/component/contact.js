ee12c437554af54081b89cbb79265378cb3920ee
Fionalan727 <lanyufei727@gmail.com>
2019-09-03 23:51:45 -0400

    summary to contact page

--- fiona-portfolio/src/component/contact.js
+++ fiona-portfolio/src/component/contact.js
@@ -6,7 +6,14 @@ class Contact extends Component {
         return(
             <div className="contact-body">
                <Grid className="contact-grid">
-                    <Cell col={6}>Half Page</Cell>
+                    <Cell col={6}>
+                        <h2>Yufei Lan</h2>
+                        <img src="./images/profile.jpg"
+                            alt="avatar"
+                            style={{height:'307px',borderRadius:'8%'}}
+                        />
+                        <p style={{width:'75%',margin:'auto',paddingTop:'1em'}}>Full stack developer who is passionate about problem solving and logical puzzles . Enjoys building awesome websites that combine functionality with efficiency to create an intuitive and beautiful user experience. Strong team player driven by results with efficiency. </p>
+                    </Cell>
                     <Cell col={6}>Half Page</Cell>
 
                </Grid>
