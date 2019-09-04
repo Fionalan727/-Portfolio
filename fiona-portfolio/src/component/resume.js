cad1b946759111da37a41e1a2616570edb8ac88b
Fionalan727 <lanyufei727@gmail.com>
2019-09-04 18:08:21 -0400

    add profile in resume page

--- fiona-portfolio/src/component/resume.js
+++ fiona-portfolio/src/component/resume.js
@@ -6,7 +6,18 @@ class Resume extends Component {
         return(
             <div>
                 <Grid>
-                    <Cell col={4}>Left Side</Cell>
+                    <Cell col={4}>
+                        <div style={{textAlign:'center'}}>
+                            <img 
+                                src="./images/profile.jpg"
+                                alt="avatar"
+                                style={{height:'200px',borderRadius:"50%"}}
+                            />
+                        </div>
+                        <h2 style={{paddingTop:'2em'}}>Yufei Lan</h2>
+                        <h4 style={{color:'grey'}}>Full Stack Web Developer</h4>
+                        <hr style={{borderTop:'3px solid #833fb2', width:'50%'}}/>
+                    </Cell>
                     <Cell col={8} className="resume-right-col">Right Side</Cell>
                 </Grid>
             </div>
